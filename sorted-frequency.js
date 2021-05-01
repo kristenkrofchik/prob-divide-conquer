e8eead81e147102730aea6c45b7c2fe4c4d7d108
Kristen Krofchik <k.krofchik@gmail.com>
2021-05-01 09:42:17 -0500

    in process

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,17 @@
-function sortedFrequency() {
+function sortedFrequency(arr, num) {
+    let lowI = 0;
+    let highI = arr.length - 1;
 
+    if(highI >= lowI) {
+        let midI = Math.floor((lowI + highI) / 2);
+        if(arr[midI] > num) {
+            highI = midI - 1; 
+        }
+        if(arr[midI] < num) {
+            lowI = midI + 1;
+        }
+        if(arr[midI] === num && (arr[mi]))
+    }
 }
 
 module.exports = sortedFrequency
