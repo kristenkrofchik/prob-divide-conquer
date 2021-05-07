c7c69a781d025f3db2eac875a70f90271c7344e3
Kristen Krofchik <k.krofchik@gmail.com>
2021-05-07 10:02:53 -0500

    working on find floor binary search

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,23 @@
-function findFloor() {
-  
+function findFloor(arr, x) {
+  let leftI = 0;
+  let rightI = arr.length - 1;
+
+  while(leftI <= rightI) {
+      midI = Math.floor((leftI + rightI) / 2);
+      midVal = arr[midI];
+
+      if(midVal > x) {
+          rightI = midI - 1;
+      }
+
+      if(midVal < x ) {
+        leftI = midI + 1;
+      }
+
+      if(midVal === x || midVal !== x && arr.length === 1) {
+          return midVal;
+      }
+  }
 }
 
 module.exports = findFloor
