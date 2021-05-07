4425909704ea6aa5c1d4014a4bd5456664d86983
Kristen Krofchik <k.krofchik@gmail.com>
2021-05-07 09:39:30 -0500

    working on freq of sorted arr binary search

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,17 +1,26 @@
 function sortedFrequency(arr, num) {
     let lowI = 0;
     let highI = arr.length - 1;
+    let firstI 
+    let lastI
 
-    if(highI >= lowI) {
-        let midI = Math.floor((lowI + highI) / 2);
-        if(arr[midI] > num) {
-            highI = midI - 1; 
-        }
-        if(arr[midI] < num) {
+    while(lowI <= highI) {
+        midI = Math.floor((highI + lowI) / 2);
+        midVal = arr[midI];
+        if(midVal < num) {
             lowI = midI + 1;
         }
-        if(arr[midI] === num && (arr[mi]))
+        if(midVal > num) {
+            highI = midVal - 1;
+        }
+        if(midVal === num && arr[midI - 1] != num || midI === 0 && midVal === num) {
+            firstI = midI;
+        }
+        if(midVal === num && arr[midI + 1] != num || midI === arr.length - 1 && midVal === num) {
+            lastI = midI;
+        }
     }
+    return lastI - firstI;
 }
 
 module.exports = sortedFrequency
