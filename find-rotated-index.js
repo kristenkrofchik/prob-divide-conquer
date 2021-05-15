9cfe0e6c724b7772803ec98b0c734fb76cc70797
Kristen Krofchik <k.krofchik@gmail.com>
2021-05-14 22:28:14 -0500

    passing find-rotated-index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,22 +1,35 @@
 function findRotatedIndex(arr, num) {
     let pivot = findPivot(arr);
-    if (pivot > 0 && num >= array[0] && num <= array[pivot - 1]) {
-        return binarySearch(array, num, 0, pivot - 1);
+    if (pivot > 0 && num >= arr[0] && num <= arr[pivot - 1]) {
+        return binarySearch(arr, num, 0, pivot - 1);
       } else {
-        return binarySearch(array, num, pivot, array.length - 1);
+        return binarySearch(arr, num, pivot, arr.length - 1);
       }
     }
 
-binarySearch(arr, num, start, end) {
+function binarySearch(arr, num, start, end) {
     if(arr.length === 0) {
         return -1;
     }
-    if (num < array[start] || num > array[end]) {
+    if (num < arr[start] || num > arr[end]) {
         return -1;
     }
-} 
 
-findPivot(arr) {
+    while (start <= end) {
+        var mid = Math.floor((start + end) / 2);
+        if (arr[mid] === num) {
+          return mid;
+        } else if (num < arr[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+      return -1;
+    }
+
+
+function findPivot(arr) {
     if(arr.length === 1) {
         return 0;
     }
@@ -37,6 +50,5 @@ findPivot(arr) {
     }
 }
 
-}
 
 module.exports = findRotatedIndex
